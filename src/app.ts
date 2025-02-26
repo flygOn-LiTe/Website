adb00a2aceedccec0622ff992ac23b8302beae7a
Andrew Rowberry <rowbs.js@gmail.com>
2025-02-25 20:55:17 -0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,81 +1,86 @@
-import path from 'path';
+//commit go!
 
-import Fastify from 'fastify';
-import Autoload from '@fastify/autoload';
-import FormBody from '@fastify/formbody';
-import Multipart from '@fastify/multipart';
-import Static from '@fastify/static';
-import View from '@fastify/view';
-import Cookie from '@fastify/cookie';
-import Session from '@fastify/session';
-import RateLimit from '@fastify/rate-limit';
-import ejs from 'ejs';
+import path from "path";
 
-import Environment from '#/util/Environment.js';
+import Fastify from "fastify";
+import Autoload from "@fastify/autoload";
+import FormBody from "@fastify/formbody";
+import Multipart from "@fastify/multipart";
+import Static from "@fastify/static";
+import View from "@fastify/view";
+import Cookie from "@fastify/cookie";
+import Session from "@fastify/session";
+import RateLimit from "@fastify/rate-limit";
+import ejs from "ejs";
+
+import Environment from "#/util/Environment.js";
 
 const fastify = Fastify({
-    // logger: true
+  // logger: true
 });
 
 fastify.register(FormBody);
 fastify.register(Multipart);
 
 fastify.register(Static, {
-    root: path.join(process.cwd(), 'public')
+  root: path.join(process.cwd(), "public"),
 });
 
 fastify.register(View, {
-    engine: {
-        ejs
-    },
-    root: path.join(process.cwd(), 'view'),
-    viewExt: 'ejs'
+  engine: {
+    ejs,
+  },
+  root: path.join(process.cwd(), "view"),
+  viewExt: "ejs",
 });
 
 fastify.register(Cookie);
 fastify.register(Session, {
-    secret: Environment.SESSION_SECRET,
-    cookie: {
-        secure: false
-    }
+  secret: Environment.SESSION_SECRET,
+  cookie: {
+    secure: false,
+  },
 });
 
 await fastify.register(RateLimit, {
-    max: 100,
-    timeWindow: 1000 * 5
+  max: 100,
+  timeWindow: 1000 * 5,
 });
 
-fastify.setNotFoundHandler({
+fastify.setNotFoundHandler(
+  {
     preHandler: fastify.rateLimit({
-        max: 5,
-        timeWindow: 1000 * 5
-    })
-}, (request, reply) => {
+      max: 5,
+      timeWindow: 1000 * 5,
+    }),
+  },
+  (request, reply) => {
     reply.status(404).send();
-});
+  }
+);
 
 fastify.setErrorHandler(function (error, request, reply) {
-    if (error.validation) {
-        reply.status(400).send('');
-    }
-})
+  if (error.validation) {
+    reply.status(400).send("");
+  }
+});
 
 await fastify.register(Autoload, {
-    dir: 'src/routes',
-    forceESM: true
+  dir: "src/routes",
+  forceESM: true,
 });
 
 fastify.listen(
-    {
-        port: Environment.WEB_PORT,
-        host: '0.0.0.0'
-    },
-    (err: Error | null, address: string) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-
-        console.log(`[Web]: Listening on port ${Environment.WEB_PORT}`);
+  {
+    port: Environment.WEB_PORT,
+    host: "0.0.0.0",
+  },
+  (err: Error | null, address: string) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
     }
+
+    console.log(`[Web]: Listening on port ${Environment.WEB_PORT}`);
+  }
 );
