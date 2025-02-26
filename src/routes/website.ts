aa8460c10a60b35affa5369c430b24072c36ae3c
Andrew Rowberry <rowbs.js@gmail.com>
2025-02-26 14:48:33 -0800

    chore: checking title route

--- src/routes/website.ts
+++ src/routes/website.ts
@@ -1,99 +1,98 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance } from "fastify";
 
-import { db } from '#/db/query.js';
+import { db } from "#/db/query.js";
 
-import Environment from '#/util/Environment.js';
-import WorldList from '#/util/WorldList.js';
+import Environment from "#/util/Environment.js";
+import WorldList from "#/util/WorldList.js";
 
 export default async function (app: FastifyInstance) {
-    app.get('/', async (req, reply) => {
-        return reply.view('index');
+  app.get("/", async (req, reply) => {
+    return reply.view("index");
+  });
+
+  app.get("/disclaimer", async (req, reply) => {
+    return reply.view("disclaimer");
+  });
+
+  app.get("/title", async (req, reply) => {
+    return reply.view("title", {
+      "hello world": "Hello World",
     });
+  });
 
-    app.get('/disclaimer', async (req, reply) => {
-        return reply.view('disclaimer');
-    });
+  interface QueryServerList {
+    method: number;
+    "hires.x"?: number;
+    "hires.y"?: number;
+    "lores.x"?: number;
+    "lores.y"?: number;
+  }
 
-    app.get('/title', async (req, reply) => {
-        let playerCount = 0;
-        for (let world of WorldList) {
-            playerCount += world.players;
-        }
+  app.get<{
+    Querystring: QueryServerList;
+  }>(
+    "/serverlist",
+    {
+      schema: {
+        querystring: {
+          type: "object",
+          properties: {
+            "hires.x": { type: "number" },
+            "hires.y": { type: "number" },
+            "lores.x": { type: "number" },
+            "lores.y": { type: "number" },
+            method: { type: "number" },
+          },
+          required: ["method"],
+        },
+      },
+    },
+    async (req, reply) => {
+      let members = WorldList.filter((x) => x.members).length;
+      let regions = {
+        "United States": "us",
+        Germany: "ger",
+        Australia: "au",
+        Japan: "jp",
+        Russia: "rus",
+        India: "ind",
+        Sweden: "swe",
+        "Local Development": "uk",
+      };
+      let freeRegions = WorldList.filter((x) => x.region && !x.members)
+        .map((x) => x.region)
+        .filter((x, i, self) => self.indexOf(x) == i);
+      let membersRegions = WorldList.filter((x) => x.region && x.members)
+        .map((x) => x.region)
+        .filter((x, i, self) => self.indexOf(x) == i);
 
-        const latestNews = Environment.DB_HOST ? await db.selectFrom('newspost').orderBy('id', 'desc').limit(5).selectAll().execute() : [];
-        return reply.view('title', {
-            playerCount,
-            newsposts: latestNews
-        });
-    });
+      if (req.query.method == 4) {
+        return reply.redirect(
+          "https://github.com/LostCityRS/Launcher/releases/latest"
+        );
+      }
 
-    interface QueryServerList {
-        method: number;
-        'hires.x'?: number;
-        'hires.y'?: number;
-        'lores.x'?: number;
-        'lores.y'?: number;
+      return reply.view("serverlist", {
+        detail: typeof req.query["hires.x"] !== "undefined" ? "high" : "low",
+        method: req.query.method,
+        worlds: WorldList,
+        members,
+        regions,
+        freeRegions,
+        membersRegions,
+      });
     }
+  );
 
-    app.get<{
-        Querystring: QueryServerList
-    }>('/serverlist', {
-        schema: {
-            querystring: {
-                type: 'object',
-                properties: {
-                    'hires.x': { type: 'number' },
-                    'hires.y': { type: 'number' },
-                    'lores.x': { type: 'number' },
-                    'lores.y': { type: 'number' },
-                    'method': { type: 'number' },
-                },
-                required: ['method']
-            }
-        }
-    }, async (req, reply) => {
-        let members = WorldList.filter(x => x.members).length;
-        let regions = {
-            'United States': 'us',
-            Germany: 'ger',
-            Australia: 'au',
-            Japan: 'jp',
-            Russia: 'rus',
-            India: 'ind',
-            Sweden: 'swe',
-            'Local Development': 'uk'
-        };
-        let freeRegions = WorldList.filter(x => x.region && !x.members)
-            .map(x => x.region)
-            .filter((x, i, self) => self.indexOf(x) == i);
-        let membersRegions = WorldList.filter(x => x.region && x.members)
-            .map(x => x.region)
-            .filter((x, i, self) => self.indexOf(x) == i);
+  app.get("/detail", async (req, reply) => {
+    return reply.view("detail");
+  });
 
-        if (req.query.method == 4) {
-            return reply.redirect('https://github.com/LostCityRS/Launcher/releases/latest');
-        }
+  app.get("/worldmap", async (req, reply) => {
+    return reply.view("worldmap/applet");
+  });
 
-        return reply.view('serverlist', {
-            detail: typeof req.query['hires.x'] !== 'undefined' ? 'high' : 'low',
-            method: req.query.method,
-            worlds: WorldList,
-            members,
-            regions,
-            freeRegions,
-            membersRegions
-        });
-    });
-
-    app.get('/detail', async (req, reply) => {
-        return reply.view('detail');
-    });
-
-    app.get('/worldmap', async (req, reply) => {
-        return reply.view('worldmap/applet');
-    });
-
-    app.get('/guide', async (req, reply) => {
-        return reply.view('guide');
-    });
+  app.get("/guide", async (req, reply) => {
+    return reply.view("guide");
+  });
 }
